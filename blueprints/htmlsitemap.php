818c85fd1445f21a26a1b11f1b80d5466a3108c4
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2015-04-06 21:52:26 +0200

    updated blueprints

--- blueprints/htmlsitemap.php
+++ blueprints/htmlsitemap.php
@@ -2,16 +2,29 @@
 
 # sitemap blueprint
 
-title: do not touch - for sitemap only
+title: x- only for html-sitemap
 pages: false
 files: false
 
 fields:
+  info:
+    label: 
+      de:  HINWEIS
+    type:  info
+    text:  >
+      This site generates a html-base sitemap (a human readable one)
+      <br>
+      You should probably set some of the following options in the config.php
+      <br>
+      <pre>c::set('smap_ignoreSite', array(...));</pre> 
+      <pre>c::set('smap_ignoreTemplate', array(...));</pre>
+      <pre>c::set('smap_heading_visible', '...');</pre>
+      <pre>c::set('smap_heading_invisible', '...');</pre>
+      <pre>c::set('smap_column_class_id', 'class="..." id="..."');</pre>
+      
+      For more information see the readme.
+
   title:
     label: Title
       de:  Titel/ Überschrift
     type:  text
-  meta_robots:
-    label: meta-robots
-    type:  text
-    default: noindex, nofollow
--- blueprints/xmlsitemap.php
+++ blueprints/xmlsitemap.php
@@ -2,12 +2,26 @@
 
 # sitemap blueprint
 
-title: do not touch - for sitemap only
+title: x- only for xmlsitemap
 pages: false
 files: false
 
 fields:
-  meta_robots:
-    label: meta-robots
+  info:
+    label: 
+      de:  HINWEIS
+    type:  info
+    text:  >
+      This site generates a html-base sitemap (a human readable one). It is recommended, that this site is available under the URL /sitemap!
+      <br>
+      You should probably set some of the following options in the config.php
+      <br>
+      <pre>c::set('smap_ignoreSite', array(...));</pre> 
+      <pre>c::set('smap_ignoreTemplate', array(...));</pre>
+      
+      For more information see the readme.
+
+  title:
+    label: Title (has no fuction)
+      de:  Seitenüberschrift (hat keine Funktion)
     type:  text
-    default: noindex, nofollow
